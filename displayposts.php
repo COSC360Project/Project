36705d3a6c59f0ca193bfcde675f3885a555ed93
denisek0216 <denisek0216@gmail.com>
2021-04-10 07:49:16 -0700

    Added documentation and other minor changes.

--- displayposts.php
+++ displayposts.php
@@ -9,44 +9,45 @@
             exit($errmsg);
         }
         else {
-            // get post results from db
-            $sql = "SELECT * FROM blogpost ORDER BY date DESC";
-            if (array_key_exists('category', $_POST)) {
-                $sql = "SELECT * FROM blogpost ORDER BY category DESC";
+            // Define SQL to display post results
+            $sqlDisplay = "SELECT * FROM blogpost ORDER BY date DESC";
+			// Change SQL if sort-by category option is selected
+			if (isset($_POST['category'])) {		// Old code: if (array_key_exists('category',$_POST)) 
+                $sqlDisplay = "SELECT * FROM blogpost ORDER BY category DESC";
             }
-            $result = mysqli_query($conn, $sql);
-                    
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
+            $displayResult = mysqli_query($conn, $sqlDisplay);
+            
+			// Display results       
+            if (mysqli_num_rows($displayResult) > 0) {
+                while ($row = mysqli_fetch_assoc($displayResult)) {
                     echo "<div class='post-entry'>";
                     echo "<h2>".$row['title']."&nbsp;by&nbsp;".$row['authorid']."</h2>";
                     echo "<h5>Date posted:&nbsp;".$row['date']."</h5>";
-                    echo "<p>".$content."</p>";
+                    echo "<p>".$row['content']."</p>";
                     echo "</div>";
                }
             }
             else {
                     echo "<p>0 results returned.</p>";
-                    exit();
             }
-
+			
+			// If user requests a search
             if (isset($_POST['search'])) {
                 $keyword = $_POST['search-keyword'];
-                $sqlsearch = "SELECT * FROM blogpost WHERE title LIKE %$keyword% ORDER BY date DESC";
-                $searchresult = mysqli_query($conn,$sqlsearch);
+                $sqlSearch = "SELECT * FROM blogpost WHERE title LIKE %".$keyword."% ORDER BY date DESC";
+                $searchresult = mysqli_query($conn,$sqlSearch);
                 if (mysqli_num_rows($searchresult)> 0) {
                     while ($searchrow = mysqli_fetch_assoc($searchresult)) {
                         echo "<div class='post-entry'>";
                         echo "<h2>".$row['title']."&nbsp;by&nbsp;".$row['authorid']."</h2>";
                         echo "<h5>Date posted:&nbsp;".$row['date']."</h5>";
-                        echo "<p>".$content."</p>";
+                        echo "<p>".$row['content']."</p>";
                         echo "</div>";
                     }
                 }
-                else {
-                    echo "<p>0 results returned.</p>";
-                    exit();
             }
+            else {
+                    echo "<p>Search query '".$keyword."' returned 0 results.</p>";
             }
         }
         // close connection
