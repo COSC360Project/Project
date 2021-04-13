400ebab7d8bf96c9767a1aed5a06b412113db486
denisek0216 <denisek0216@gmail.com>
2021-04-13 02:56:06 -0700

    Added links to header.php

--- header.php
+++ header.php
@@ -1,34 +1,34 @@
 <?php
-		
+	Header ("Content-type: text/css; charset=utf-8");
 	if (isset($_SESSION["status"])){
 		$status = $_SESSION["status"];
 		if ($status == 1){
 			//Admin header
-			echo "<h1>MyBlogPost</h1>";
+			echo "<h1><a href='home.php'>MyBlogPost</a></h1>";
 			echo "<article id=\"right-icon\"><img class=\"thumbnail\" src='".$_SESSION["imageurl"]."'/></article>";
 			echo "<table id=\"box\"><tbody><tr>";
 			echo "<td><a href=\"home.php\">Home</a></td>";
 			echo "<td><a href=\"#\">My Posts</a></td>";
-			echo "<td><a href=\"#\">New Post</a></td>";
+			echo "<td><a href=\"createpostpage.php\">New Post</a></td>";
 			echo "<td><a href=\"adminsite.php\">Administrator</a></td>";
 			echo "<td><a href=\"userinfo.php\">My Account</a></td>";
 			echo "<td><a href=\"logout.php\">Logout</a></td>";
 			echo "</tr></tbody></table>";
 		}else if($status == 0){
 			//User header
-			echo "<h1>MyBlogPost</h1>";
+			echo "<h1><a href='home.php'>MyBlogPost</a></h1>";
 			echo "<article id=\"right-icon\"><img class=\"thumbnail\" src='".$_SESSION["imageurl"]."'/></article>";
 			echo "<table id=\"box\"><tbody><tr>";
 			echo "<td><a href=\"home.php\">Home</a></td>";
 			echo "<td><a href=\"#\">My Posts</a></td>";
-			echo "<td><a href=\"#\">New Post</a></td>";
+			echo "<td><a href=\"createpostpage.php\">New Post</a></td>";
 			echo "<td><a href=\"#\"></a></td>";
 			echo "<td><a href=\"userinfo.php\">My Account</a></td>";
 			echo "<td><a href=\"logout.php\">Logout</a></td>";
 			echo "</tr></tbody></table>";
 		}else if($status == -1){
 			//banned user, unregistered user header
-			echo "<h1>MyBlogPost</h1>";
+			echo "<h1><a href='home.php'>MyBlogPost</a></h1>";
 			echo "<article id=\"right-icon\"><img class=\"thumbnail\" src='images/blank.png'/></article>";
 			echo "<table id=\"box\"><tbody><tr>";
 			echo "<td><a href=\"home.php\">Home</a></td>";
@@ -40,7 +40,7 @@
 			echo "</tr></tbody></table>";
 		}
 	}else{
-		echo "<h1>MyBlogPost</h1>";
+		echo "<h1><a href='home.php'>MyBlogPost</a></h1>";
 		echo "<article id=\"right-icon\"><img class=\"thumbnail\" src='images/blank.png'/></article>";
 		echo "<table id=\"box\"><tbody><tr>";
 		echo "<td><a href=\"home.php\">Home</a></td>";
